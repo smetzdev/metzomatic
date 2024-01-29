c8fd886aadde954682ded50656c1739d16c8a561
Sascha Metz <hello@smetz.dev>
2024-01-29 20:16:18 +0100

    refactor(gmail): :fire: remove controller
    gmail does not need to get exposed per an http api anymore

--- src/gmail/gmail.module.ts
+++ src/gmail/gmail.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GmailService } from './gmail.service';
-import { GmailController } from './gmail.controller';
 
 @Module({
   providers: [GmailService],
-  controllers: [GmailController],
 })
 export class GmailModule {}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,28 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { GmailService } from './../src/gmail/gmail.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let gmailService: GmailService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(GmailService)
-      .useValue({
-        sendMail: jest.fn(),
-      })
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe());
     await app.init();
-
-    gmailService = moduleFixture.get<GmailService>(GmailService);
   });
 
   describe('toggl', () => {
@@ -36,47 +26,6 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer())
         .get('/toggl/reports/last-month/12345')
         .expect(200);
-    });
-  });
-
-  describe('gmail', () => {
-    it('/send (POST) should work with correct body', async () => {
-      const requestBody = {
-        to: 'test@test.com',
-        subject: 'Test Subject',
-        text: 'Test Body',
-      };
-      const expectedResponseText = 'Email sent successfully';
-
-      const response = await request(app.getHttpServer())
-        .post('/gmail/send')
-        .send(requestBody)
-        .expect(201);
-
-      expect(gmailService.sendMail).toHaveBeenCalledWith(requestBody);
-
-      expect(response.text).toEqual(expectedResponseText);
-    });
-
-    it('/send (POST) should not work with malformed body', async () => {
-      // With a wrong email
-      const requestBody = {
-        to: 'wrongemail.com',
-        subject: 'Test Subject',
-        text: 'Test Body',
-      };
-      const expectedBody = {
-        message: ['to must be an email'],
-        error: 'Bad Request',
-        statusCode: 400,
-      };
-
-      const response = await request(app.getHttpServer())
-        .post('/gmail/send')
-        .send(requestBody)
-        .expect(400);
-
-      expect(response.body).toEqual(expectedBody);
     });
   });
 });
