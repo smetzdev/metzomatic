99ca5fc2dfd8bc98f0f68609a4f6d0e2026fed9b
Sascha Metz <hello@smetz.dev>
2024-01-29 20:43:09 +0100

    test(toggl): :white_check_mark: add controller specs

--- src/toggl/toggl.controller.spec.ts
+++ src/toggl/toggl.controller.spec.ts
@@ -4,6 +4,7 @@ import { TogglService } from './toggl.service';
 
 describe('TogglController', () => {
   let controller: TogglController;
+  let service: TogglService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +12,45 @@ describe('TogglController', () => {
       providers: [
         {
           provide: TogglService,
-          useValue: {},
+          useValue: {
+            getLastMonthReport: jest.fn(),
+            getCurrentMonthReport: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     controller = module.get<TogglController>(TogglController);
+    service = module.get<TogglService>(TogglService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getLastMonthReport', () => {
+    it('should call the getLastMonthReport method of TogglService and return the result', async () => {
+      // Arrange
+      const clientId = '123';
+
+      // Act
+      await controller.getLastMonthReport(clientId);
+
+      // Assert
+      expect(service.getLastMonthReport).toHaveBeenCalledWith(+clientId);
+    });
+  });
+
+  describe('getCurrentMonthReport', () => {
+    it('should call the getCurrentMonthReport method of TogglService and return the result', async () => {
+      // Arrange
+      const clientId = '123';
+
+      // Act
+      await controller.getCurrentMonthReport(clientId);
+
+      // Assert
+      expect(service.getCurrentMonthReport).toHaveBeenCalledWith(+clientId);
+    });
   });
 });
